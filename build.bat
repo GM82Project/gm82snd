2306b0a4468c2e431170ed082b19d7a1c33233be
omicronrex <65692652+omicronrex@users.noreply.github.com>
2022-11-13 19:47:22 -0300

    add dll source and update build tools

--- build.bat
+++ build.bat
@@ -1,1 +1,16 @@
+call "%VS120COMNTOOLS%vsvars32.bat"
+
+del GMFMODSimple.dll
+
+cd GMFMODSimpleGrix
+
+msbuild GMFMODSimpleGrix.sln /property:Configuration=Release /p:WarningLevel=0
+
+copy Release\GMFMODSimpleGrix.dll ..\GMFMODSimple.dll
+
+cd ..
+
+if exist GMFMODSimple.dll (
 build_gex.py gm82snd.gej
+)
+pause
