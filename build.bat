6a78fdaf77b8751d4b09f44846cd407c699972fa
Floogle <18466542+skyfloogle@users.noreply.github.com>
2023-05-16 19:30:24 +0200

    switch to cmake

--- build.bat
+++ build.bat
@@ -1,16 +1,3 @@
-call "%VS120COMNTOOLS%vsvars32.bat"
+cmake -B build -A Win32 -DINSTALL_GEX=ON && cmake --build build --config Release
 
-del GMFMODSimple.dll
-
-cd GMFMODSimpleGrix
-
-msbuild GMFMODSimpleGrix.sln /property:Configuration=Release /p:WarningLevel=0
-
-copy Release\GMFMODSimpleGrix.dll ..\GMFMODSimple.dll
-
-cd ..
-
-if exist GMFMODSimple.dll (
-build_gex.py gm82snd.gej
-)
 pause
